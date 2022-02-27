d20d7b999aa839b9de620fa7cbab2c1a9c299d7c
Pikhosh <73311467+pikhosh@users.noreply.github.com>
2022-02-27 19:43:53 +0330

    Update Sefareshi.py

--- Sefareshi.py
+++ Sefareshi.py
@@ -492,5 +492,19 @@ pre {
 """, js_code="""$('footer').html('طراح: <a href="https://pikhosh.blog.ir" target="blank" title="وبلاگ طراح قالب">پیخوش</a> | قدرت گرفته از <a href="https://www.pyweb.io/" target="_blank">PyWebIO</a>')""")
 
 
+if __name__ == '__main__':
+    import argparse
+    from pywebio.platform.tornado_http import start_server as start_http_server
+    from pywebio import start_server as start_ws_server
 
-start_server(main, port=8080, websocket_ping_interval=30)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    parser.add_argument("--http", action="store_true", default=False, help='Whether to enable http protocol for communicates')
+    args = parser.parse_args()
+
+    if args.http:
+        start_http_server(main, port=args.port)
+    else:
+        # Since some cloud server may close idle connections (such as heroku),
+        # use `websocket_ping_interval` to  keep the connection alive
+        start_ws_server(main, port=args.port, websocket_ping_interval=30)
